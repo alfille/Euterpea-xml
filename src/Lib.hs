ba33279bb50dfbe95ef9f1089828fb0eee278d9d
alfille <paul.alfille@gmail.com>
2025-05-11 22:39:29 -0400

    Update stack files for some libraries

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     ) where
+
+import qualified Data.Text.IO as T
+import qualified Euterpea as E
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
